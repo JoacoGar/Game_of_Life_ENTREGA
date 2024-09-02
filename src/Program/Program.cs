f52b80b319fd9b3ae083ee3119c4fee38d05acce
GuilleGuerrico098 <guillermo.guerrico@correo.ucu.edu.uy>
2024-09-01 21:02:42 -0300

    Terminado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,28 +1,24 @@
-<<<<<<< Updated upstream
-﻿using System;
-=======
-﻿//La clase Program contiene el método Main, cumplen con el principio de  responsabilidad única, es el
+//La clase Program contiene el método Main, cumplen con el principio de  responsabilidad única, es el
 //punto de entrada del programa.
 using System;
 using System.Threading;
->>>>>>> Stashed changes
 
-namespace Ucu.Poo.GameOfLife
+namespace Ucu.Poo.GameOfLife;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-<<<<<<< Updated upstream
-        static void Main(string[] args)
-=======
         FileReader lectorArchivo = new FileReader(); 
         bool[,] estadoInicial = lectorArchivo.LeerBlocDeNotas("board.txt");
         Board tablero = new Board(estadoInicial);
         Logic logicaJuego = new Logic();
         PrintBoard impresoraTablero = new PrintBoard();
         while (true)
->>>>>>> Stashed changes
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            impresoraTablero.Imprimir(tablero);
+            logicaJuego.ActualizarTablero(tablero);
+            Thread.Sleep(300);
         }
     }
 }
